7f1ffcacf49a51d2f8a976d6188d9506d8b4a9ca
Junyoung Shin <jun021222@naver.com>
2023-01-04 22:53:02 +0900

    publish version 1

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-copy = document.querySelector(".copy")
-copy.addEventListener("click", function(event){
-    test = event
+copyList = document.querySelectorAll(".copy")
+copyList.forEach(
+    (copy) => copy.addEventListener("click", function (event) {
+        test = event
 
-    var tempElem = document.createElement('textarea');
-    tempElem.value = event.srcElement.innerText;  
-    document.body.appendChild(tempElem);
-  
-    tempElem.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempElem);
-  });
+        var tempElem = document.createElement('textarea');
+        tempElem.value = event.srcElement.innerText;
+        document.body.appendChild(tempElem);
+
+        tempElem.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempElem);
+    })
+)
